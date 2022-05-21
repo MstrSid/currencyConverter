e7e862109cd6202ea5c21ed1dbf0720ddfd55b3e
Eugene Kos <mstrsid@live.com>
2022-05-21 17:23:49 +0300

    first commit

---
+++ src/js/informers/cursBBNal.js
@@ -0,0 +1,21 @@
+export function cursBBNal(city = 'Минск') {
+	let currArray = ['USD', 'EUR', 'RUB', 'PLN'];
+	return getDataFromApi(city, currArray);
+}
+
+async function getDataFromApi(city, currArray) {
+	return await fetch(`https://belarusbank.by/api/kursExchange?city=${city}`)
+		.then(result => result.json())
+		.then(data => {
+			const res = {};
+			for (let key in data[0]) {
+				currArray.forEach(item => {
+					if (`${key}` === `${item}_in` || `${key}` === `${item}_out`) {
+						res[key] = data[0][key];
+					}
+				});
+			}
+			return res;
+		})
+		.catch(e => e.toString());
+}
---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+import {cursBBNal} from "./informers/cursBBNal";
+
+cursBBNal('Бобруйск').then(data => console.log(data));
